0e4b2c4334fd12c969ae1c2a6362ac51ac437a3e
lakhara <siddharth.lakhara@gmail.com>
2018-01-16 22:36:37 +0530

    prgrm 6 -> test2 passed

--- count_reduce.js
+++ count_reduce.js
@@ -22,10 +22,10 @@ function verifyInput(retOutput, testInput){
 
 test0 = countWords(["apple", "mango", "apple", "banana"]);
 console.log("works for base case: ",verifyInput(test0, { apple: 2, mango: 1, banana: 1 }) === true);
-/*
+
 test1 = countWords(["apple", "apple", "apple", "apple"]);
 console.log("Variation in base case: ",verifyInput(test1, { apple: 4 }) === true);
-
+/*
 test2 = countWords([]);
 console.log("Empty array: ",verifyInput(test2, { }) === true);
 
